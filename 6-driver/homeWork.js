9be64babe7355c18854944b2a9cb874230286a6b
Anton <antkolomytsev@gmail.com>
2026-01-31 21:52:33 +0300

    Исправлено на тернарный оператор

--- 6-driver/homeWork.js
+++ 6-driver/homeWork.js
@@ -2,5 +2,5 @@ const hasLicence = true;
 const age = 20;
 const isDrink = false;
 
-const isCanDrive = hasLicence && age >= 18 && !isDrink;
+const isCanDrive = (hasLicence && age >= 18 && !isDrink) ? 'Может' : 'Не может';
 console.log(isCanDrive);
